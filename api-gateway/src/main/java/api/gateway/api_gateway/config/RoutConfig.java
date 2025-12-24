61992f416e5e67bd255e0a0999c932f4beb8d229
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-12-24 14:22:29 +0800

    setup routing

---
+++ api-gateway/src/main/java/api/gateway/api_gateway/config/RoutConfig.java
@@ -0,0 +1,17 @@
+package api.gateway.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("auth-service", r -> r.path("/api/auth/**").uri("http://localhost:8081"))
+            .build();
+    }
+    
+}
